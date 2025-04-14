ef7f8bd36fc0f4b4d85cfecea8f67ba84f961f21
MykyBalb <mykaelbalbino06@gmail.com>
2025-04-14 16:42:18 -0300

    atualizações ex1

--- prova/Assets/ex1.js
+++ prova/Assets/ex1.js
@@ -8,21 +8,19 @@ function mult5() {
     let num1Input = document.getElementById("num1");
     let num2Input = document.getElementById("num2");
     let resultado = document.getElementById('resultado');
-    let mult = 5;
+    let multiplicador = 5;
     let multiplos = 0;
 
     if (num1Input instanceof HTMLInputElement && num2Input instanceof HTMLInputElement && resultado != null  ) {
         let num1 = parseInt(num1Input.value);
         let num2 = parseInt(num2Input.value);
-        let max = Math.max(num1, num2); //pega o maior numero entre os dois
-        let min = Math.min(num1, num2); //pega o menor
+        let maior = Math.max(num1, num2); //pega o maior numero entre os dois
+        let menor = Math.min(num1, num2); //pega o menor
 
-        for(min; min <= max; min++ ) {
-            let result = mult%min;
-            console.log(min);
-            if (result <= 0) {
-                multiplos++;
-                console.log(multiplos);
+        for(menor; menor <= maior; menor++ ) { //add +1 ao menor numero ate ficar equivalente ao maior e vai divindo enquanto isso
+            let result = multiplicador%menor; //pega o resto da multiplicação entre 5 e o menor numero
+            if (result == 0) { //se o resto for 0
+                multiplos++; //entao adiciona mais um múltiplo
             }
         }
         resultado.textContent= `A quantidade de múltiplos é de ${multiplos}`;
